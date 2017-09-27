cc36a420a5fcbce4a4d01698aeb55c9a29e63bbe
kongchenglc@gmail.com <kongchenglc@gmail.com>
2017-09-27 12:30:32 +0800

    Destructuring

--- Destructuring.js
+++ Destructuring.js
@@ -1,4 +1,5 @@
 //变量的解构赋值
+//只要等号右边的值不是对象或数组，就先将其转为对象。
 
 //数组解构赋值
     //解构赋值属于模式匹配，按照对应关系对变量赋值
@@ -63,5 +64,53 @@
         let [x = y, y = 1] = [];     // ReferenceError
 
 
-//对象
+//对象的解构赋值
+    var { foo, bar } = { foo: "aaa"};
+    var { bar, foo } = { foo: "aaa", bar:"bbb" };
+    //对象的解构赋值的内部机制，是先找到同名属性，然后再赋给对应的变量。真正被赋值的是后者，而不是前者。
 
+        let { y } = { y: 1 };
+        //相当于下边这句，定义了一个变量y，可以在外边访问到y
+        let { y: y } = { y: 1 };
+        //而这句只能在外边访问到a变量，不能访问y。
+        let { y: a } = { y: 1 };
+
+        //此处f是变量，而first只是模式，按照模式来寻找如何赋值，被赋值的是变量
+            let obj = { first: 'hello', last: 'world' };
+            let { first: f, last: l } = obj;
+            f // 'hello'
+            l // 'world'
+
+        //如果要将一个已经声明的变量用于解构赋值，要小心
+            // 错误的写法，{x}引擎会将大括号开头的代码理解为代码块
+            let x;
+            { x } = { x: 1 };
+            // 正确的写法
+            let x;
+            ({ x } = { x: 1 });
+        //内置对象也可以进行解构赋值
+            let { log, sin, cos } = Math;
+
+
+//字符串的解构赋值
+    //字符串也可以解构赋值。这是因为此时，字符串被转换成了一个类似数组的对象。
+        const [a, b, c, d, e] = 'hello';
+        a // "h"
+        b // "e"
+        c // "l"
+        d // "l"
+        e // "o"
+    //类似数组的对象都有一个length属性，因此还可以对这个属性解构赋值。
+        let { length: len } = 'hello';
+        len // 5
+
+
+//数字和布尔值会被转成对象然后解构
+    let { toString: s } = 123;
+    s === Number.prototype.toString // true
+
+    let { toString: s } = true;
+    s === Boolean.prototype.toString // true
+
+    
+//
